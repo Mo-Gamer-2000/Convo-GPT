23cc3217871581577dcff5d12298e68f8d4d12b2
Moeez Abdul <moeezrajput2000@gmail.com>
2023-12-07 22:49:07 +0000

    Working on Routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import { config } from "dotenv";
+import morgan from "morgan";
+import appRouter from "./routes/index.js";
+
 config();
 const app = express();
 
 // Middlewares
 app.use(express.json());
+
+// *-- MUST BE REMOVED BEFORE *PRODUCTION* --*
+app.use(morgan("dev"));
+
+app.use("/api/v1", appRouter);
 
 export default app;
