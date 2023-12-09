0c6faa2f016709fe83b3be49007d60c94f0c7d60
Moeez Abdul <moeezrajput2000@gmail.com>
2023-12-09 23:13:25 +0000

    User SignUp, Route & Controller, Encrypted & Tested (Bcrypt & Postman)

--- backend/src/controllers/user.controllers.ts
+++ backend/src/controllers/user.controllers.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
+import { hash } from "bcrypt";
 
+// Get all of the Users
 export const getAllUsers = async (
   req: Request,
   res: Response,
@@ -9,6 +11,29 @@ export const getAllUsers = async (
   try {
     const users = await User.find();
     return res.status(200).json({ message: "OK", users });
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
+};
+
+// User Sign Up
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await hash(password, 10);
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+
+    return res.status(200).json({ message: "OK", id: user._id.toString() });
   } catch (error) {
     console.log(error);
     return res.status(200).json({ message: "ERROR", cause: error.message });
