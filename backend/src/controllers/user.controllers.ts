dbcd0843cc79e7dfba260bef5fd7199f0890f1c2
Moeez Abdul <moeezrajput2000@gmail.com>
2023-12-13 10:25:36 +0000

    Token Created

--- backend/src/controllers/user.controllers.ts
+++ backend/src/controllers/user.controllers.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
 import { hash, compare } from "bcrypt";
+import { createToken } from "../utils/token-manager.js";
 
 // Get all of the Users
 export const getAllUsers = async (
@@ -59,6 +60,8 @@ export const userLogin = async (
     if (!isPasswordCorrect) {
       return res.status(403).send("Invalid password");
     }
+
+    const token = createToken(user._id.toString(), user.email, "7d");
 
     return res.status(200).json({ message: "OK", id: user._id.toString() });
   } catch (error) {
