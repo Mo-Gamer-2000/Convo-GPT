05faeffb3ed4a43d1304f6e8cdb059ee1f8c9f7b
Moeez Abdul <moeezrajput2000@gmail.com>
2023-12-15 11:18:15 +0000

    Neccessary Packages Installed Routes & Pages Set Up - CSS Reset - Rafactored Code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  typography: {
+    fontFamily: "Roboto Slab, serif",
+    allVariants: { color: "white" },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
