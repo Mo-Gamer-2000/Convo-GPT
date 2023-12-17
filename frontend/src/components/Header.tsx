7a4e40af5d949b083f0acdceb57653e6469e4603
Moeez Abdul <moeezrajput2000@gmail.com>
2023-12-17 14:38:49 +0000

    Navigation Bar Design Complete || Based on user isLoggedIn state or Not.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,51 @@
 import { AppBar, Toolbar } from "@mui/material";
 import React from "react";
 import Logo from "./shared/Logo";
+import { useAuth } from "../context/AuthContext";
+import NavigationLink from "./shared/NavigationLink";
 
 const Header = () => {
+  const auth = useAuth();
   return (
     <AppBar
       sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
     >
       <Toolbar sx={{ display: "flex" }}>
         <Logo />
+        <div>
+          {auth?.isLoggedIn ? (
+            <>
+              <NavigationLink
+                bg="gold"
+                to="/chat"
+                text="Chat to Convo"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="white"
+                to="/"
+                text="Logout"
+                textColor="black"
+                onClick={() => auth.logout()}
+              />
+            </>
+          ) : (
+            <>
+              <NavigationLink
+                bg="white"
+                to="/login"
+                text="Login"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="gold"
+                to="/signup"
+                text="Signup"
+                textColor="black"
+              />
+            </>
+          )}
+        </div>
       </Toolbar>
     </AppBar>
   );
