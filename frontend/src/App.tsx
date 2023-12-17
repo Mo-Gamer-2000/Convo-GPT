1c04fff27e9d149d5c0a3ad6906604bffdd145de
Moeez Abdul <moeezrajput2000@gmail.com>
2023-12-17 14:09:36 +0000

    State Management with Context API for the User

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,11 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Chat from "./pages/Chat";
 import NotFound from "./pages/NotFound";
+import { useAuth } from "./context/AuthContext";
 
 function App() {
+  console.log(useAuth()?.isLoggedIn);
+
   return (
     <main>
       <Header />
