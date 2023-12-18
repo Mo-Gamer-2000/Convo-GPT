23b50098ecff16cf38866cdcd3661d05909e9f37
Moeez Abdul <moeezrajput2000@gmail.com>
2023-12-18 10:49:15 +0000

    Wokring on Login Page Design

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { Box } from "@mui/material";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Box width={"100%"} height={"100%"} display="flex" flex={1}>
+      <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
+        <img src="Logo.webp" alt="Logo" style={{ width: "400px" }} />
+      </Box>
+      <Box></Box>
+    </Box>
+  );
 };
 
 export default Login;
