1905bcfed2ec89ab7534617fba447d543306f44c
Moeez Abdul <moeezrajput2000@gmail.com>
2023-12-19 11:07:08 +0000

    Login Page Design Complete & Responsive

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,12 +1,70 @@
-import { Box } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import CustomisedInput from "../components/shared/CustomisedInput";
+import { IoMdLogIn } from "react-icons/io";
 
 const Login = () => {
   return (
     <Box width={"100%"} height={"100%"} display="flex" flex={1}>
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
-        <img src="Logo.webp" alt="Logo" style={{ width: "400px" }} />
+        <img src="LoginRobot.webp" alt="Robot" style={{ width: "400px" }} />
       </Box>
-      <Box></Box>
+      <Box
+        display={"flex"}
+        flex={{ xs: 1, md: 0.5 }}
+        justifyContent={"center"}
+        alignItems={"center"}
+        padding={2}
+        marginLeft={"auto"}
+        marginTop={16}
+      >
+        <form
+          style={{
+            margin: "auto",
+            padding: "30px",
+            boxShadow: "10px 10px 20px #000",
+            borderRadius: "10px",
+            border: "none",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <Typography
+              variant="h4"
+              textAlign="center"
+              padding={2}
+              fontWeight={600}
+            >
+              Login
+            </Typography>
+            <CustomisedInput type="email" name="email" label="Email" />
+            <CustomisedInput type="password" name="password" label="Password" />
+            <Button
+              type="submit"
+              sx={{
+                px: 2,
+                py: 1,
+                mt: 2,
+                width: "400px",
+                borderRadius: 2,
+                bgcolor: "gold",
+                color: "black",
+                ":hover": {
+                  bgcolor: "white",
+                  color: "black",
+                },
+              }}
+              endIcon={<IoMdLogIn />}
+            >
+              Login
+            </Button>
+          </Box>
+        </form>
+      </Box>
     </Box>
   );
 };
