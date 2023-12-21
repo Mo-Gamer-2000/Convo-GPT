022ee753c185b308f8ac1174927308ab91cfc23b
Moeez Abdul <moeezrajput2000@gmail.com>
2023-12-21 11:07:48 +0000

    Axios for Communication between Backend & fronend: Implemented User Login API Request & React-Hot-Toast used to Display Notifications.

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -5,6 +5,7 @@ import {
   useEffect,
   useState,
 } from "react";
+import { loginUser } from "../helpers/api-communicator";
 
 type User = {
   name: string;
@@ -27,7 +28,13 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   useEffect(() => {
     // Fetch if the User's Cookies are Valid then skip Login.
   }, []);
-  const login = async (email: string, password: string) => {};
+  const login = async (email: string, password: string) => {
+    const data = await loginUser(email, password);
+    if (data) {
+      setUser({ email: data.email, name: data.name });
+      setIsLoggedIn(true);
+    }
+  };
   const signup = async (name: string, email: string, password: string) => {};
   const logout = async () => {};
 
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,16 +1,24 @@
 import { Box, Button, Typography } from "@mui/material";
 import CustomisedInput from "../components/shared/CustomisedInput";
+import { toast } from "react-hot-toast";
 import { IoMdLogIn } from "react-icons/io";
+import { useAuth } from "../context/AuthContext";
 
 const Login = () => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const auth = useAuth();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const email = formData.get("email");
-    const password = formData.get("password");
-    console.log("====================================");
-    console.log(email, password);
-    console.log("====================================");
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    try {
+      toast.loading("Signing In", { id: "login" });
+      await auth?.login(email, password);
+      toast.success("Signed In Successfully", { id: "login" });
+    } catch (error) {
+      console.log(error);
+      toast.error("Signing In was Unsuccessfull", { id: "login" });
+    }
   };
   return (
     <Box width={"100%"} height={"100%"} display="flex" flex={1}>
