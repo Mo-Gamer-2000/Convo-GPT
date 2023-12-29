ace83e2209a3ed42ff883de7e476908b1fe039f3
Moeez Abdul <moeezrajput2000@gmail.com>
2023-12-29 13:37:45 +0000

    Basic 404 NotFound Component Design Created

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,5 +1,41 @@
-const NotFound = () => {
-  return <div>NotFound</div>;
+import React, { FC } from "react";
+
+interface NotFoundProps {
+  message?: string;
+  suggestion?: string;
+}
+
+const NotFound: FC<NotFoundProps> = ({
+  message = "The page you're looking for might be under construction.",
+  suggestion = "Please check the URL or try again later.",
+}) => {
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.heading}>404 - Not Found</h1>
+      <p style={styles.message}>{message}</p>
+      <p style={styles.suggestion}>{suggestion}</p>
+    </div>
+  );
+};
+
+const styles: { [key: string]: React.CSSProperties } = {
+  container: {
+    textAlign: "center",
+    marginTop: "50px",
+  },
+  heading: {
+    fontSize: "2em",
+    color: "#d9534f", // Bootstrap's danger color
+  },
+  message: {
+    fontSize: "1.2em",
+    marginTop: "20px",
+  },
+  suggestion: {
+    fontSize: "1em",
+    marginTop: "10px",
+    color: "#777",
+  },
 };
 
 export default NotFound;
